743cfdb5f91631c893cfd4bd960524fefe5c883d
Luce5in3 <2655768716@qq.com>
2025-12-25 14:58:49 +0800

    feat: 1.表主键 自己构造

--- backend/src/main/java/com/graduate/certificate/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/graduate/certificate/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.graduate.certificate.common.exception;
 
 import com.graduate.certificate.common.result.Result;
 import com.graduate.certificate.common.result.ResultCode;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.stream.Collectors;
 
 /**
